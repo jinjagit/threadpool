2e7df99189cae1bb421ba05d7e7748648921ace8
Simon Tharby <simontharby@gmail.com>
2024-10-20 19:54:14 +0100

    add Rayon example, using 6 threads

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,7 +4,6 @@
 
 extern crate num_cpus;
 
-use std::io::{stdin, stdout, Write};
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
@@ -14,109 +13,98 @@ fn main() {
     // Count logical cores this process could try to use.
     let num_cores = num_cpus::get();
 
-    println!("number of logical cores available: {}\n", num_cores);
-    println!("Find sum(sine(1), sine(2), sine(3) ... sine(n)), where n = 50000000\n");
+    println!("number of logical cores available: {}", num_cores);
+    println!("Spawning 6 threads (total = 7, including main)");
+    println!("Find sum(sine(1), sine(2), sine(3) ... sine(n)), where n = 50_000_000\n");
 
-    // Infinte loop: Ctrl-C to break & exit.
-    loop {
-        // Get number of threads for thread-pool from user input.
-        let num_threads = get_num_threads();
+    let num_threads = 6;
 
-        let start_time = Instant::now();
+    let start_time = Instant::now();
 
-        // Counter used to divide work between threads.
-        let thread_counter = Arc::new(AtomicU32::new(0));
-        let thread_counter_clone = thread_counter.clone();
-        // Counter used to register thread completion(s).
-        let completed_counter = Arc::new(AtomicU32::new(0));
-        let completed_counter_clone = completed_counter.clone();
+    // Counter used to divide work between threads.
+    let thread_counter = Arc::new(AtomicU32::new(0));
+    let thread_counter_clone = thread_counter.clone();
+    // Counter used to register thread completion(s).
+    let completed_counter = Arc::new(AtomicU32::new(0));
+    let completed_counter_clone = completed_counter.clone();
 
-        // Mutex 64 bit float for threads to add result(s) to.
-        let sum: Arc<Mutex<f64>> = Arc::new(Mutex::new(0.0));
-        let sum_clone = sum.clone();
+    // Mutex 64 bit float for threads to add result(s) to.
+    let sum: Arc<Mutex<f64>> = Arc::new(Mutex::new(0.0));
+    let sum_clone = sum.clone();
 
-        // Set up a thread-pool with the number of threads specified by user input.
-        let pool = ThreadPool::new(num_threads as u8);
+    // Set up a thread-pool with the number of threads specified by user input.
+    let pool = ThreadPool::new(num_threads as u8);
 
-        // Closure containing the work to be done in each thread.
-        let foo = move || {
-            let task_index = thread_counter_clone.fetch_add(1, Ordering::SeqCst);
-            let default_task_size = 50000000 / num_threads;
-            let start = default_task_size * task_index; // This method can be adjusted to set various vars for each thread. E.g. name of face (might be only thing needed!)
-            let mut result: f64 = 0.0;
+    // Closure containing the work to be done in each thread.
+    let foo = move || {
+        let task_index = thread_counter_clone.fetch_add(1, Ordering::SeqCst);
 
-            if task_index == num_threads - 1 {
-                for i in start + 1..50000001 {
-                    result += (i as f64).sin();
-                }
-            } else {
-                for i in start + 1..(default_task_size * (task_index + 1)) + 1 {
-                    result += (i as f64).sin();
-                }
-            }
+        let result = task(task_index, num_threads);
 
-            let mut sum = sum_clone.lock().unwrap();
-            *sum += result;
-            completed_counter_clone.fetch_add(1, Ordering::SeqCst);
-        };
+        let mut sum = sum_clone.lock().unwrap();
+        *sum += result;
+        completed_counter_clone.fetch_add(1, Ordering::SeqCst);
+    };
 
-        // Start the threads, passing in the closure containing the work for each.
-        for _ in 0..num_threads {
-            pool.execute(foo.clone());
-        }
-
-        let mut completed_threads_count = completed_counter.load(Ordering::SeqCst);
-
-        // Loop until every spawned thread reports completion of allocated task.
-        while completed_threads_count < num_threads {
-            completed_threads_count = completed_counter.load(Ordering::SeqCst);
-        }
-
-        let elapsed = start_time.elapsed();
-
-        println!("sum = {:.6}", sum.lock().unwrap());
-        println!("{:?} ms\n", elapsed.as_millis());
+    // Start the threads, passing in the closure containing the work for each.
+    for _ in 0..num_threads {
+        pool.execute(foo.clone());
     }
+
+    let mut completed_threads_count = completed_counter.load(Ordering::SeqCst);
+
+    // Loop until every spawned thread reports completion of allocated task.
+    while completed_threads_count < num_threads {
+        completed_threads_count = completed_counter.load(Ordering::SeqCst);
+    }
+
+    let elapsed = start_time.elapsed();
+
+    println!("sum = {:.6}", sum.lock().unwrap());
+    println!("Threadpool: {:?} ms\n", elapsed.as_millis());
+
+    run_rayon();
 }
 
-fn get_input() -> String {
-    let mut s = String::new();
-    print!("Split calculation across how many threads?\nEnter an integer from 1 to 12: ");
-    let _ = stdout().flush();
-    stdin()
-        .read_line(&mut s)
-        .expect("Did not enter a correct string");
-    if let Some('\n') = s.chars().next_back() {
-        s.pop();
-    }
-    if let Some('\r') = s.chars().next_back() {
-        s.pop();
+fn task(task_index: u32, num_threads: u32) -> f64 {
+    let default_task_size = 50000000 / num_threads;
+    let start = default_task_size * task_index; // This method can be adjusted to set various vars for each thread. E.g. name of face (might be only thing needed!)
+    let mut result: f64 = 0.0;
+
+    if task_index == num_threads - 1 {
+        for i in (start + 1)..50000001 {
+            result += (i as f64).sin();
+        }
+    } else {
+        for i in (start + 1)..(default_task_size * (task_index + 1)) + 1 {
+            result += (i as f64).sin();
+        }
     }
 
-    s
+    result
 }
 
-fn get_num_threads() -> u32 {
-    let mut i: u32 = 127;
+fn run_rayon() {
+    let start_time = Instant::now();
 
-    while i > 12 {
-        let input = get_input();
+    // run task in parallel using rayon
+    let mut result_a: f64 = 0.0;
+    let mut result_b: f64 = 0.0;
+    let mut result_c: f64 = 0.0;
+    let mut result_d: f64 = 0.0;
+    let mut result_e: f64 = 0.0;
+    let mut result_f: f64 = 0.0;
+    rayon::scope(|s| {
+        s.spawn(|_| result_a = task(0, 6));
+        s.spawn(|_| result_b = task(1, 6));
+        s.spawn(|_| result_c = task(2, 6));
+        s.spawn(|_| result_d = task(3, 6));
+        s.spawn(|_| result_e = task(4, 6));
+        s.spawn(|_| result_f = task(5, 6));
+    });
+    let sum = result_a + result_b + result_c + result_d + result_e + result_f;
 
-        i = match input.parse::<u32>() {
-            Ok(i) => {
-                if i > 0 && i < 13 {
-                    i
-                } else {
-                    println!("\nERROR! Try again");
-                    127
-                }
-            }
-            Err(_) => {
-                println!("\nERROR! Try again");
-                127
-            }
-        };
-    }
-
-    i
+    println!("sum = {:.6}", sum);
+    let elapsed = start_time.elapsed();
+    println!("Rayon: {:?} ms\n", elapsed.as_millis());
 }
